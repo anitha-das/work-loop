ac3376a906f818a4cbd5f6a775d1e7ced0637a42
Anitha Das <anithadas680@gmail.com>
2026-05-13 15:18:29 +0530

    changes in the cors

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const httpServer = createServer(app);
 
 app.use(cors({
   origin:["http://localhost:5173",
-          "https://work-loop-anitha-das-projects.vercel.app/"],
+          "https://work-loop-anitha-das-projects.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"]
